981b6ff9b7576558641dc9c06ebbd6a51a79fb40
IsaacPachecoooo <isaac@genotipo.com>
2026-02-04 13:08:42 -0600

    fix: Corregir etiqueta de cierre HTML en TaskTable

--- components/TaskTable.tsx
+++ components/TaskTable.tsx
@@ -95,7 +95,7 @@ const TaskTable: React.FC<Props> = ({ tasks, allTasks, role, onUpdateTask, onVie
                 <td className="px-6 py-6">
                               <div className={`text-base font-bold text-gray-800 whitespace-normal break-words leading-tight ${ task.status === 'Completada' ? 'line-through opacity-60' : '' }`}>
               {task.title}
-            </div>td>
+            </div>td></
                 <td className="px-6 py-6">
                   <span className={`text-[11px] font-black uppercase whitespace-nowrap ${task.deliveryDate ? 'text-brand-dark' : 'text-gray-300 italic'}`}>
                     {formatDeliveryDate(task.deliveryDate)}
